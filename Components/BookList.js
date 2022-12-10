a50cfe67a825d68443e4818d948450b476308a22
h21mieri <h21mieri@du.se>
2022-12-10 02:01:17 +0100

    styling

--- Components/BookList.js
+++ Components/BookList.js
@@ -1,5 +1,5 @@
 const BookList = (bookList) => {
-  let html = `<ul class="ring-4 bg-slate-200 w-1/2 place-self-center rounded-md p-2 text-center test book-list mx-auto">`;
+  let html = `<ul class="ring-4 bg-slate-200 w-1/2 place-self-center rounded-md p-2 mx-auto text-center test book-list">`;
   for (let i = 0; i < bookList.length; i++) {
     html += BookListItem(bookList[i]);
   }
--- Components/InfoCard.js
+++ Components/InfoCard.js
@@ -1,20 +1,23 @@
 const InfoCard = (book) => {
   let html = `
-              <div class="info-card"> 
-                <div class="rounded-md border-2 border-black bg-gray-400 w-full mx-auto w-60">
-                 <div class="p-3">
-                  <img src= "${book.coverImage}"/>
-                    <div>----------------------------------</div>
-                      <div>Author: ${book.author}</div>
-                        <div>Title: ${book.title}</div>
-                          <div>Pages: ${book.pages}</div>
-                            <div>Release Date: ${book.releaseDate}</span>
-                          <div>
-                        <div>
-                      </div> 
-                    </div>
-                  </div>
-                  </div>
-              </div>`;
+  <div
+    class="info-card rounded-[1rem] overflow-hidden text-white bg-cover pt-[10rem] pb-0 pl-0 pr-0 max-w-[35ch]"
+    style="background-image: url('${book.coverImage}'), linear-gradient(to bottom right, indigo, black)">
+    <div
+      class="card-content p-[1.5rem] bg-gradient-to-t from-black via-black/80 to-black/0">
+      <h2
+        class="font-black uppercase tracking-widest card-title relative w-max after:absolute after:h-[4px] after:left-0 after:bottom-0 after:w-full after:bg-green-300">
+        ${book.title}
+      </h2>
+      <p
+        class="card-body text-white/85 pt-[0.5rem] pb-[0.5rem]">
+        ${book.title} was written by ${book.author} in ${book.releaseDate}.
+      </p>
+      <div
+        class="button cursor-pointer inline-block no-underline text-black bg-green-300 py-[0.5em] px-[1.5em]">
+        Length: ${book.pages} pages
+      </div>
+    </div>
+  </div>`;
   return html;
 };
