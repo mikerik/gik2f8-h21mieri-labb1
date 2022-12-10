b0e8a26b23c66a17cb51b10b50e1f67ee0eb43ef
h21mieri <h21mieri@du.se>
2022-12-10 03:07:35 +0100

    Updated api.js

--- api/api.js
+++ api/api.js
@@ -1,16 +1,19 @@
 const url = "https://gik2f8-labs.herokuapp.com/books";
 
 async function getAll() {
-  const result = await fetch(url)
-    .then((result) => result.json())
-    .catch((e) => e);
-
-  return result;
+  try {
+    const result = await fetch(url);
+    return await result.json();
+  } catch (e) {
+    return e;
+  }
 }
 
 async function getOne(id) {
-  const result = await fetch(url + "/" + id)
-    .then((result) => result.json())
-    .catch((e) => e);
-  return result;
+  try {
+    const result = await fetch(`${url}/${id}`);
+    return await result.json();
+  } catch (e) {
+    return e;
+  }
 }
