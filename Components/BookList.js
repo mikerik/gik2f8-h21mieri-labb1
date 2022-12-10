ccf1c075711f91b3a8a9b3f1f9c32efa0443f22d
h21mieri <h21mieri@du.se>
2022-12-10 03:02:15 +0100

    Updated BookList.js

--- Components/BookList.js
+++ Components/BookList.js
@@ -1,5 +1,5 @@
 const BookList = (bookList) => {
-  let html = `<ul class="ring-4 bg-slate-200 w-1/2 place-self-center rounded-md p-2 mx-auto text-center test book-list">`;
+  let html = `<ul class="ring-4 bg-slate-100 w-1/2 place-self-center rounded-md p-2 mx-auto text-center test book-list">`;
   for (let i = 0; i < bookList.length; i++) {
     html += BookListItem(bookList[i]);
   }
