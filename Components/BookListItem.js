3d93a3d5a2adbbc68e89127689782aaa0fb850da
h21mieri <h21mieri@du.se>
2022-12-10 03:03:32 +0100

    Updated BookListItem.js

--- Components/BookListItem.js
+++ Components/BookListItem.js
@@ -1,6 +1,6 @@
 const BookListItem = (book) => {
   let html = `<li
-                class="book-list__item ring-4 bg-slate-400 p-3 m-3 rounded-md hover:bg-white hover:shadow-2xl hover:shadow-indigo-500/40 transition delay-100 ease-in-out cursor-pointer" id="${book.id}">
+                class="book-list__item ring-4 bg-slate-300 p-3 m-3 rounded-md hover:bg-white hover:shadow-2xl hover:shadow-indigo-500/40 transition delay-100 ease-in-out cursor-pointer" id="${book.id}">
                 ${book.author} - ${book.title} 
               </li>`;
   return html;
